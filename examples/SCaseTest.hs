6b3b964450f54b7cb5b3d75e08b6a4bf5815e942
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-06-05 13:21:43 +0200

    Add example for SynchronousCase

---
+++ examples/SCaseTest.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE LambdaCase #-}
+module SCaseTest where
+
+import CLaSH.Prelude
+import CLaSH.Signal.SynchronousCase
+
+topEntity :: Signal (Maybe Integer) -> Signal Integer
+topEntity x = match x $ \case
+  Just_ a en  -> let s = regEn 0 en (a + s) in s
+  Nothing_ en -> -1
+
+testInput :: Signal (Maybe Integer)
+testInput = match oscillate
+  $ \case
+  { True_ en -> let s = regEn 0 en (s + 1) in Just <$> s
+  ; False_ _ -> signal Nothing
+  }
+  where
+    oscillate = let s = register False (not1 s) in s
