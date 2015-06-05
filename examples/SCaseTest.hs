b70cfa7bc0a4b1003352f2ab37ef31c0bfa47024
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-06-05 13:54:29 +0200

    Further simplify example

--- examples/SCaseTest.hs
+++ examples/SCaseTest.hs
@@ -10,10 +10,8 @@ topEntity x = match x $ \case
   Nothing_ en -> -1
 
 testInput :: Signal (Maybe Integer)
-testInput = match oscillate
-  $ \case
-  { True_ en -> let s = regEn 0 en (s + 1) in Just <$> s
-  ; False_ _ -> signal Nothing
-  }
+testInput = match oscillate $ \case
+    True_ en -> let s = regEn 0 en (s + 1) in Just <$> s
+    False_ _ -> signal Nothing
   where
     oscillate = register False (not <$> oscillate)
