06b34e44dfd602dc78f0632b89dc013ac949e2b4
Christiaan Baaij <christiaan.baaij@gmail.com>
2015-06-05 13:59:04 +0200

    Further simplify example

--- examples/SCaseTest.hs
+++ examples/SCaseTest.hs
@@ -4,14 +4,20 @@ module SCaseTest where
 import CLaSH.Prelude
 import CLaSH.Signal.SynchronousCase
 
+-- | Oscillate between @False@ and @True@ every cycle
+oscillate = register False (not <$> oscillate)
+
+-- | Counter which increment by the value of @dIn@ when @enable@ is @True@
+counter enable dIn = cnt
+  where
+    cnt = regEn 0 enable (cnt + dIn)
+
 topEntity :: Signal (Maybe Integer) -> Signal Integer
 topEntity x = match x $ \case
-  Just_ a en  -> let s = regEn 0 en (a + s) in s
+  Just_ a en  -> counter en a
   Nothing_ en -> -1
 
 testInput :: Signal (Maybe Integer)
 testInput = match oscillate $ \case
-    True_ en -> let s = regEn 0 en (s + 1) in Just <$> s
+    True_ en -> Just <$> counter en 1
     False_ _ -> signal Nothing
-  where
-    oscillate = register False (not <$> oscillate)
